78ab01c229c7f182855e6643de7bf2b4716a6c6a
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-09 14:21:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,10 @@ def monopoly_with_third_tier
   	    :reading_railroad => {},
   	    :pennsylvania_railroad => {},
   	    :b_and_o_railroad => {},
-  	    :shortline_railroad => {}
+  	    :shortline_railroad => {},
   	    :mortgage_value => "$100"
   	  }
+  	}
   	}
 end
 
