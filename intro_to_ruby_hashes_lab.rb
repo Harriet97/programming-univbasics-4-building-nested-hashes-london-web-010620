2a2495187ff5bedd17dc0094e044837eced1a8b2
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-09 14:09:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_third_tier
   monopoly= {:railroads => 
   	{:pieces => 4,
   	  :rent_in_dollars =>{
-  	    :one_piece_owned => "£ 25",
+  	    :one_piece_owned => 25,
   	    :key2 => "",
   	    :key3 => "",
   	    :key4 => ""
